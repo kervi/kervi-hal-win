4e96ec7fc27bb724ebf46b00979f2f3b8c48ad42
Tim Wentzlau <tim@wentzlau.dk>
2018-01-14 22:05:43 +0000

    update gpio to reflect api changes

--- kervi/platforms/windows/gpio.py
+++ kervi/platforms/windows/gpio.py
@@ -1,22 +1,35 @@
 from kervi.hal.gpio import IGPIODeviceDriver
 
+
 class GPIODriver(IGPIODeviceDriver):
 
-    GPIO2 = 3
-    GPIO3 = 5
-    GPIO4 = 7
-
-    def __init__(self):
-        #print("init win gpio driver")
+    def __init__(self, gpio_id="generic_gpio"):
+        IGPIODeviceDriver.__init__(self, gpio_id)
         pass
 
-    def define_as_input(self, pin):
+    def _get_channel_type(self, channel):
+        from kervi.hal.gpio import CHANNEL_TYPE_GPIO, CHANNEL_TYPE_ANALOG_IN, CHANNEL_TYPE_ANALOG_OUT
+        if channel in ["GPIO1", "GPIO2", "GPIO3"]:
+            return CHANNEL_TYPE_GPIO
+        elif channel in ["DAC1", "DAC2"]:
+            return CHANNEL_TYPE_ANALOG_OUT
+        elif channel in ["ADC1", "ADC2"]:
+            return CHANNEL_TYPE_ANALOG_IN
+
+    def _get_channel_names(self):
+        return ["GPIO1", "GPIO2", "GPIO3", "DAC1", "DAC2", "ADC1", "ADC2"]
+
+    @property
+    def name(self):
+        return "Generic GPIO"
+
+    def define_as_input(self, pin, pullup=None, bounce_time=0):
         print("define pin in")
 
     def define_as_output(self, pin):
         print("define pin out")
 
-    def define_as_pwm(self, pin, frequency):
+    def define_as_pwm(self, pin, frequency, duty_cycle):
         print("define pwm")
 
     def set(self, pin, state):
